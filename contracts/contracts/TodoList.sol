d57b160fbd01816e2a7fbd292edc49b3cd9e623f
dplamenov <dimitar.plamenov@gmail.com>
2022-04-24 17:31:34 +0300

    TodoList contract

---
+++ contracts/contracts/TodoList.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract TodoList {
+    struct Todo {
+        string todo;
+    }
+
+    mapping(address => Todo[]) todos;
+
+    function addTodo(string memory _todo) public {
+        return todos[msg.sender].push(Todo(_todo));
+    }
+}
