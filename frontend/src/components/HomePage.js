7431e47fa676c9ae7e052c0599edc692d24ddfae
dplamenov <dimitar.plamenov@gmail.com>
2022-04-25 13:59:49 +0300

    table

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -12,28 +12,38 @@ function HomePage() {
 
 
   useEffect(() => {
-    if (!window.ethereum) {
-      console.log("please install MetaMask")
-      return
-    }
-
-    window.ethereum.request({ method: 'eth_requestAccounts' })
+   window.ethereum.request({ method: 'eth_requestAccounts' })
       .then(result => {
         setCurrentAccount(result[0]);
+
+        provider.getBalance(result[0])
+          .then(ethers.utils.formatEther)
+          .then(setBalance);
+        
         contract.getTodos().then(setTodos);
-        provider.getBalance(result[0]).then(ethers.utils.formatEther).then(setBalance);
+        contract.getTodos().then(console.log);
       });  
   }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const value = e.target.todo.value;
+    const todo = e.target.todo.value;
+    const description = e.target.description.value;
 
-    contract.addTodo(value).then(_ => {
+    contract.addTodo(todo, description).then(() => {
       e.target.todo.value = '';
-      setTodos(todos => [...todos, {todo: value}]);
+      e.target.description.value = '';
+      setTodos(todos => [...todos, {todo, description, _id: todos.length}]);
     });
   };
+
+  const removeHandler = (id) => {
+    return () => {
+      contract.removeTodo(id).then(() => {
+        setTodos(todos => todos.filter((todo, key) => key !== id));
+      });
+    }
+  }
 
   return (
     <div style={{width: "70%", margin: "0 auto"}}>
@@ -41,14 +51,35 @@ function HomePage() {
       <p>Account: {currentAccount}</p>
       <p>Balance: {balance} ETH</p>
       <hr />
-      <h2>Todos</h2>
-      {todos.map((todo, key) => {
-        return <p key={key}>{todo.todo}</p>
-      })}
+      <h2>Create new todo</h2>
       <form onSubmit={handleSubmit}>
         <input placeholder='Todo' name='todo' />
+        <input placeholder='Description' name='description' />
         <button>Create</button>
       </form>
+      <h2>Todos</h2>
+      <table style={{width: '100%', textAlign: 'center'}}>
+        <thead>
+          <tr>
+            <th>Todo</th>
+            <th>Description</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo, key) => {
+            return (
+              <tr key={key}>
+                <td>{todo.todo}</td>
+                <td>{todo.description}</td>
+                <td>
+                  <button onClick={removeHandler(key)}>X</button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
