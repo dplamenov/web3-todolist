08fc01908a651fdb0869d1effe1d3a6a2e987de2
dplamenov <dimitar.plamenov@gmail.com>
2022-04-28 11:50:09 +0300

    use beforeEach in test file

--- contracts/test/sample-test.js
+++ contracts/test/sample-test.js
@@ -2,29 +2,25 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("TodoList", function () {
-  it("should be empty", async function () {
-    const TodoList = await ethers.getContractFactory("TodoList");
-    const todoList = await TodoList.deploy();
-    await todoList.deployed();
+  let todoList;
 
+  beforeEach(async () => {
+    const TodoList = await ethers.getContractFactory("TodoList");
+    todoList = await TodoList.deploy();
+    await todoList.deployed();
+  })
+
+  it("should be empty", async function () {
     expect(await todoList.getTodos()).to.deep.equal([]);
   });
 
   it("should add todo in list", async function () {
-    const TodoList = await ethers.getContractFactory("TodoList");
-    const todoList = await TodoList.deploy();
-    await todoList.deployed();
-
     await todoList.addTodo("simple todo", "1");
 
     expect(await todoList.getTodos()).to.deep.equal([["simple todo", "1"]]);
   });
 
   it("should add todos in list", async function () {
-    const TodoList = await ethers.getContractFactory("TodoList");
-    const todoList = await TodoList.deploy();
-    await todoList.deployed();
-
     await todoList.addTodo("simple todo", "2");
     await todoList.addTodo("my new todo", "3");
 
@@ -32,10 +28,6 @@ describe("TodoList", function () {
   });
 
   it("should delete todos from list", async function () {
-    const TodoList = await ethers.getContractFactory("TodoList");
-    const todoList = await TodoList.deploy();
-    await todoList.deployed();
-
     await todoList.addTodo("simple todo", "2");
     await todoList.addTodo("my new todo", "3");
     expect(await todoList.getTodos()).to.deep.equal([["simple todo", "2"], ["my new todo", "3"]]);
@@ -48,10 +40,6 @@ describe("TodoList", function () {
   });
 
   it("should edit todo", async function () {
-    const TodoList = await ethers.getContractFactory("TodoList");
-    const todoList = await TodoList.deploy();
-    await todoList.deployed();
-
     await todoList.addTodo("simple todo", "1");
     await todoList.addTodo("tickets", "to buy");
 
