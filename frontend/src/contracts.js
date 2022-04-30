dcf16e0183c282791651bbd6709111ee538632a3
dplamenov <dimitar.plamenov@gmail.com>
2022-04-30 11:07:33 +0300

    contract

--- frontend/src/contracts.js
+++ frontend/src/contracts.js
@@ -1,7 +1,7 @@
 import TodoListABI from './abi/TodoList.json'
 
 const contracts = {
-  'TodoList': { abi: TodoListABI, address: '0x5FbDB2315678afecb367f032d93F642f64180aa3' }
+  'TodoList': { abi: TodoListABI, address: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9' }
 };
 
 export default contracts
